a203d98caf263e5f996aa46f7a17c4543725e756
Adrian Michel <adrian@yloader.com>
2018-02-03 13:50:59 -0800

    include already in stdafx.h

--- src/updater/Updater.cpp
+++ src/updater/Updater.cpp
@@ -17,7 +17,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "stdafx.h"
 #include "Updater.h"
-#include <json.hpp>
 
 using json = nlohmann::json;
 using namespace yloader;
