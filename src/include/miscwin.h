a31ac916b7dd99e542c08c9db714f1dd71e5b65f
Adrian Michel <adrian@tradery.com>
2020-07-04 18:42:25 -0700

    updating copyright notice

--- src/include/miscwin.h
+++ src/include/miscwin.h
@@ -1,5 +1,26 @@
+/*
+Copyright(C) 2020  YLoader.com
+
+This program is free software : you can redistribute it and / or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #pragma once
 
+#include <Windows.h>
+#include <ostream>
+#include <sstream>
+#include <string>
 #include <assert.h>
 #include <tchar.h>
 #include <string>
@@ -137,28 +158,6 @@ MISCWIN_API const RunProcessResult
 runProcess(const std::wstring& processFileName, const std::wstring& cmdLine, bool waitForProcess, const std::wstring* startingDirectory = 0,
            const Environment& env = Environment(), unsigned long timeout = 0);
 
-
-/*
-Copyright(C) 2017  YLoader.com
-
-This program is free software : you can redistribute it and / or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.If not, see <http://www.gnu.org/licenses/>.
-*/
-
-#include <Windows.h>
-#include <ostream>
-#include <sstream>
-#include <string>
 
 template <class CharT, class TraitsT = std::char_traits<CharT> >
 class basic_debugbuf : public std::basic_stringbuf<CharT, TraitsT> {
